77aea403b35a789a776377ce752d539b3e7cd014
sirkays <attehkayode2@gmail.com>
2025-08-21 15:18:22 +0100

    Modify next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: "standalone",
   basePath: "/texagon", // 👈 your repo name
   assetPrefix: "/texagon/",
   eslint: {
