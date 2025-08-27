9c68fddb101d84ba341966185d847807d15f4e15
Olatimileyin Josiah Modupe <87277319+cyrusCreative@users.noreply.github.com>
2025-08-27 09:27:00 +0100

    My Material

--- app/student/materials/page.tsx
+++ app/student/materials/page.tsx
@@ -1,13 +1,13 @@
-'use client'
-import React from 'react'
-import { MaterialUploader } from '@/components/student/material-uploader'
+"use client";
+import {MyMaterials} from "@/components/student/my-materials";
+import React from "react";
 
 const MaterialsMain = () => {
   return (
     <div className="p-4 sm:p-6 lg:p-8 w-full">
-        <MaterialUploader />
+      <MyMaterials />
     </div>
-  )
-}
+  );
+};
 
-export default MaterialsMain
+export default MaterialsMain;
