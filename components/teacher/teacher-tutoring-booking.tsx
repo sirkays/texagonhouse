516054089ee48f553fd5503333a349d9f98300a8
Simon Peter <petersimon0730@gmail.com>
2025-10-27 16:41:02 +0100

    tutoring bookings push

--- components/teacher/teacher-tutoring-booking.tsx
+++ components/teacher/teacher-tutoring-booking.tsx
@@ -1662,7 +1662,7 @@ export function TeacherTutoringBooking() {
         </TabsContent>
       </Tabs>
 
-      <div className="grid gap-4 md:grid-cols-3">
+      {/* <div className="grid gap-4 md:grid-cols-3">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">
@@ -1717,7 +1717,7 @@ export function TeacherTutoringBooking() {
             </p>
           </CardContent>
         </Card>
-      </div>
+      </div> */}
     </div>
   );
 }
