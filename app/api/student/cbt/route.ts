a70325c248355ba6b32b68ecd664da2fa30a0945
sirkays <attehkayode2@gmail.com>
2025-10-29 11:17:46 +0100

    Update cbt teacher 1.2

--- app/api/student/cbt/route.ts
+++ app/api/student/cbt/route.ts
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-//const BASE_URL = "https://texagonbackend.epichouse.online";
-const BASE_URL = "http://127.0.0.1:9098";
+const BASE_URL = "https://texagonbackend.epichouse.online";
+//const BASE_URL = "http://127.0.0.1:9098";
 const API_KEY = "1eHxj2VU.cvTFX2nWYGyTs5HHA0CZpNJqJCjUslbz";
 
 async function fetchWithTimeout(url: string, options: any) {
