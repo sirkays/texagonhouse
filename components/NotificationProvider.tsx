f2a7652d83a2bb15543563a99accaef4145ac5ac
sirkays <attehkayode2@gmail.com>
2026-01-28 12:49:17 +0100

    Fix multiple calling of files

--- components/NotificationProvider.tsx
+++ components/NotificationProvider.tsx
@@ -9,8 +9,8 @@ export function NotificationProvider({children}: {children: React.ReactNode}) {
   useEffect(() => {
     fetchUnreadCount(); // Initial fetch on app load
 
-    const interval = setInterval(fetchUnreadCount, 45000); // Poll every 45 seconds
-    return () => clearInterval(interval);
+    // const interval = setInterval(fetchUnreadCount, 45000); // Poll every 45 seconds
+    // return () => clearInterval(interval);
   }, [fetchUnreadCount]);
 
   return <>{children}</>;
